a21c66b5bf93dfdb7bc1a704c9b7d3ba170cbc3f
CellyKlein <kleinceline@outlook.de>
2022-03-08 21:17:19 +0100

    Add model imports and export

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -1,3 +1,9 @@
 import { userModel } from './User';
+import { colorModel } from './Color';
+import { colorCollectionModel } from './ColorCollection';
 
-export default { user: { model: userModel } };
+export default { 
+    user: { model: userModel },
+    color: { model: colorModel },
+    colorCollection: { model: colorCollectionModel }
+};
